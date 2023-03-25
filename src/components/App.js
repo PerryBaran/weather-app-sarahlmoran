b2b3fbc4fe7834d83d4c4ceb06426a15aaf4ddcd
Sarah Moran <s.l.a.moran@gmail.com>
2023-03-25 11:10:53 +0000

    update of all LocationDetails references to Pascal case

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import "../styles/App.css";
 import React from "react";
-import LocationDetails from "./locationDetails";
+import LocationDetails from "./LocationDetails";
 
 function App({ location }) {
   const { city, country } = location;
