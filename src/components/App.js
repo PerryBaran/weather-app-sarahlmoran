ea193ff5d76da7f7bb1c6abb121486b160456479
Sarah Moran <s.l.a.moran@gmail.com>
2023-03-27 16:27:22 +0100

    Addition of ForecastDetails component and render in App

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,19 @@ import "../styles/App.css";
 import React from "react";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
+import ForecastDetails from "./ForecastDetails";
 
 function App({ location, forecasts }) {
   return (
     <div className="weather-app">
       <LocationDetails city={location.city} country={location.country} />
       <ForecastSummaries forecasts={forecasts} />
+      <ForecastDetails
+        date={forecasts[0].date}
+        humidity={forecasts[0].humidity}
+        temperature={forecasts[0].temperature}
+        wind={forecasts[0].wind}
+      />
     </div>
   );
 }
---
+++ src/components/ForecastDetails.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function ForecastDetails(props) {
+  const { date, humidity, temperature, wind } = props;
+  const formattedDate = new Date(date).toDateString();
+  return (
+    <div className="forecast-details" data-testid="forecast-details">
+      <div className="forecast-details__date">Date: {formattedDate}</div>
+      <div className="forecast-details__humidity">Humidity: {humidity}</div>
+      <div className="forecast-summary__mintemperature">
+        Min: {temperature.min}
+        &deg;C
+      </div>
+      <div className="forecast-summary__maxtemperature">
+        Max: {temperature.max}
+        &deg;C
+      </div>
+      <div className="forecast-details__windspeed">
+        Wind Speed: {wind.speed}
+      </div>
+      <div className="forecast-details__winddirection">
+        Wind Direction: {wind.direction}
+      </div>
+    </div>
+  );
+}
+
+export default ForecastDetails;
