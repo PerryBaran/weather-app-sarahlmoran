49aecdc779129615a507798602f47cfc19d91a4e
Sarah Moran <s.l.a.moran@gmail.com>
2023-03-29 21:30:26 +0100

    Transfer of getForecast to own file

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import "../styles/App.css";
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
 import ForecastDetails from "./ForecastDetails";
+import getForecast from "../requests/getForecast";
 
 function App() {
   const [forecasts, setForecasts] = useState([]);
@@ -18,18 +18,8 @@ function App() {
     setSelectedDate(date);
   };
 
-  const getForecast = () => {
-    const endpoint = "https://cmd-shift-weather-app.onrender.com/forecast";
-
-    axios.get(endpoint).then((response) => {
-      setSelectedDate(response.data.forecasts[0].date);
-      setForecasts(response.data.forecasts);
-      setLocation(response.data.location);
-    });
-  };
-
   useEffect(() => {
-    getForecast();
+    getForecast(setSelectedDate, setForecasts, setLocation);
   }, []);
 
   return (
