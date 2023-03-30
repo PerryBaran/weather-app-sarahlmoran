756958df16e65725338c29562fc934ec94d925c3
Sarah Moran <s.l.a.moran@gmail.com>
2023-03-30 18:32:08 +0100

    update of test following API addition

--- src/tests/components/ForecastSummaries.test.js
+++ src/tests/components/ForecastSummaries.test.js
@@ -3,35 +3,43 @@ import { render } from "@testing-library/react";
 import ForecastSummaries from "../../components/ForecastSummaries";
 
 describe("ForecastSummaries", () => {
-  const validProps = [
-    {
-      date: 11111111,
-      description: "Stub description 1",
-      icon: 800,
-      temperature: {
-        max: 22,
-        min: 12,
+  const validProps = {
+    forecasts: [
+      {
+        date: 11111111,
+        description: "Stub description 1",
+        icon: 800,
+        temperature: {
+          max: 22,
+          min: 12,
+        },
       },
-    },
-    {
-      date: 22222222,
-      description: 602,
-      icon: "stubIcon2",
-      temperature: {
-        max: 24,
-        min: 13,
+      {
+        date: 22222222,
+        description: 602,
+        icon: "stubIcon2",
+        temperature: {
+          max: 24,
+          min: 13,
+        },
       },
-    },
-  ];
+    ],
+    onForecastSelect: () => {},
+  };
 
   it("renders correctly", () => {
-    const { asFragment } = render(<ForecastSummaries forecasts={validProps} />);
+    const { asFragment } = render(
+      <ForecastSummaries
+        forecasts={validProps.forecasts}
+        onForecastSelect={validProps.onForecastSelect}
+      />
+    );
     expect(asFragment()).toMatchSnapshot();
   });
 
   it("renders the correct number of instances of ForecastSummary", () => {
     const { getAllByTestId } = render(
-      <ForecastSummaries forecasts={validProps} />
+      <ForecastSummaries forecasts={validProps.forecasts} />
     );
 
     expect(getAllByTestId("forecast-summary")).toHaveLength(2);
