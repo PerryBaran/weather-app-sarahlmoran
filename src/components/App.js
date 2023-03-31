35a6dbb79d83fc820e9c602d14351eacca0c7b64
Sarah Moran <s.l.a.moran@gmail.com>
2023-03-31 19:27:34 +0100

    addition of interactive search form, error on initial render

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,10 @@ import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
 import ForecastDetails from "./ForecastDetails";
 import getForecast from "../requests/getForecast";
+import SearchForm from "./SearchForm";
 
 function App() {
+  const [searchText, setSearchText] = useState("");
   const [forecasts, setForecasts] = useState([]);
   const [location, setLocation] = useState({ city: "", country: "" });
   const [selectedDate, setSelectedDate] = useState(0);
@@ -22,9 +24,18 @@ function App() {
     getForecast(setSelectedDate, setForecasts, setLocation);
   }, []);
 
+  const handleCitySearch = () => {
+    getForecast(searchText, setSelectedDate, setForecasts, setLocation);
+  };
+
   return (
     <div className="weather-app">
       <LocationDetails city={location.city} country={location.country} />
+      <SearchForm
+        searchText={searchText}
+        setSearchText={setSearchText}
+        onSubmit={handleCitySearch}
+      />
       <ForecastSummaries
         forecasts={forecasts}
         onForecastSelect={handleForecastSelect}
